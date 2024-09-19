b33c8d7b8a0855d3039380474695e748fd95a383
lucy sagastegui <sagasteguivalientel@iestptrujillo.net>
2024-09-19 11:02:27 -0500

    mi primer commit

---
+++ Controllers/Alumno.cs
@@ -0,0 +1,46 @@
+﻿namespace WebApplication5.Controllers
+{
+    public class Alumno
+    {
+        //Creando Propiedades
+        string Nombre;
+        string Apellido;
+        string dni;
+        DateTime fechanacimiento;
+
+        //Constructor sin parametros
+        public Alumno()
+            {
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            dni = string.Empty;
+            fechanacimiento = DateTime.Now;
+        }
+
+        //Construnctor con parametros
+        public Alumno(string pNombre, string pApellido, string pdni)
+        {
+            Nombre = pNombre;
+            Apellido = pApellido;
+            dni = pdni;
+        }
+
+        public Alumno(string pNombre, string pApellido, string pdni,DateTime pfechanacimiento)
+        {
+            Nombre = pNombre;
+            Apellido = pApellido;
+            dni = pdni;
+            fechanacimiento = pfechanacimiento;
+        }
+
+        public string DevolverDocumento()
+        {
+            return dni;
+        }
+
+        public int DevolverEdad()
+        {
+            return DateTime.Now.Year - fechanacimiento.Year;
+        }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("DevolverAlumno")]
+        public string DevolverAlumno()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno1 = new Alumno();
+            Alumno Alumno2 = new Alumno("Juan", "Perez", "4370258");
+            return Alumno2.DevolverDocumento();
+        }
+
+        [HttpGet("DevolverEdad")]
+        public int DevolverEdad()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno3 = new Alumno("Lucy", "Sagastegui", "73200723", new DateTime(2005, 10, 29));
+            return Alumno3.DevolverEdad();
+        }
+      }
+
+}
